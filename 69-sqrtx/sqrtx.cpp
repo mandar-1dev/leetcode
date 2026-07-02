9de3a8d95aebe39afa8306b4bcad724f09362019
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-07-02 13:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (13.68%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,20 +3,18 @@ public:
     int mySqrt(int x) {
         if (x < 2) return x;
 
-        int left = 1, right = x / 2;
-        int ans = 0;
+        int l = 1, r = x;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+            if (m == x / m)
+                return (m + 1 > x / (m + 1)) ? m : m + 1;
 
-            if ((long long)mid * mid <= x) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            if (m < x / m)
+                l = m + 1;
+            else
+                r = m - 1;
         }
-
-        return ans;
+        return r;
     }
 };
