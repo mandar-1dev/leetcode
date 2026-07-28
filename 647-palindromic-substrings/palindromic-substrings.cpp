852d951a3baf337ae497c70ae76850d02154ac40
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-07-28 15:25:53 +0530

    Time: 3 ms (85.66%) | Memory: 8.4 MB (91.40%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Odd-length palindromes
+            int left = i, right = i;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                ans++;
+                left--;
+                right++;
+            }
+
+            // Even-length palindromes
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+
+        return ans;
+    }
+};
