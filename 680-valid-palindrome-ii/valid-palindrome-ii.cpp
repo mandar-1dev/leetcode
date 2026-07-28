4dc726479f415b61b716ac53c9a18f8327f577a7
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-07-28 15:29:50 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (92.89%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int left, int right) {
+    bool check(string &s, int left, int right) {
         while (left < right) {
             if (s[left] != s[right])
                 return false;
-
             left++;
             right--;
         }
-
         return true;
     }
 
     bool validPalindrome(string s) {
-        int left = 0;
-        int right = s.size() - 1;
+        int left = 0, right = s.size() - 1;
 
         while (left < right) {
             if (s[left] != s[right]) {
-                return isPalindrome(s, left + 1, right) ||
-                       isPalindrome(s, left, right - 1);
+                return check(s, left + 1, right) ||
+                       check(s, left, right - 1);
             }
-
             left++;
             right--;
         }
