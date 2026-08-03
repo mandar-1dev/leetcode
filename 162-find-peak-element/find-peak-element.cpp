4ba4616a92072c722fa713ddfe1d1bf5baeea173
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-08-03 07:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (41.42%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -8,11 +8,11 @@ public:
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[mid + 1]) {
-                // Peak is at mid or on the left
+               
                 right = mid;
             } 
             else {
-                // Peak is on the right
+                
                 left = mid + 1;
             }
         }
