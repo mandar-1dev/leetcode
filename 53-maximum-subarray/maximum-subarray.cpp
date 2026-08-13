7113e40a00cb3b4c3da24a2053b218101bb4d065
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-08-13 11:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (81.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,11 +4,13 @@ public:
         int max_sum=nums[0],curr_sum=0;
         for(int val:nums){
             curr_sum+=val;
-            max_sum=max(max_sum,curr_sum);
+            max_sum=max(curr_sum,max_sum);
             if(curr_sum<0){
                 curr_sum=0;
             }
         }
+        
         return max_sum;
     }
+
 };
