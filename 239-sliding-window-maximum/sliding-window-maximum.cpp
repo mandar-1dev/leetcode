0b7cf938bba01eb72743541ac094a5926151a84e
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-08-20 18:33:41 +0530

    Time: 20 ms (69.79%) | Memory: 133.7 MB (99.81%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
+        int n = nums.size();
+
         vector<int> ans;
+        ans.reserve(n - k + 1);
 
-        for (int i = 0; i < nums.size(); i++) {
+        int* q = new int[n]; // no zero-initialization
+        int head = 0, tail = 0;
 
-            // Remove indices that are outside the current window
-            if (!dq.empty() && dq.front() <= i - k) {
-                dq.pop_front();
+        // first window
+        for (int i = 0; i < k; i++) {
+            while (head < tail && nums[q[tail - 1]] <= nums[i]) {
+                tail--;
             }
-
-            // Remove smaller elements from the back
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
-
-            // Store current index
-            dq.push_back(i);
-
-            // Start adding answers when window size becomes k
-            if (i >= k - 1) {
-                ans.push_back(nums[dq.front()]);
-            }
+            q[tail++] = i;
         }
 
+        ans.push_back(nums[q[head]]);
+
+        // remaining windows
+        for (int i = k; i < n; i++) {
+            // remove expired index
+            if (q[head] <= i - k) {
+                head++;
+            }
+
+            // remove useless smaller/equal values
+            while (head < tail && nums[q[tail - 1]] <= nums[i]) {
+                tail--;
+            }
+
+            q[tail++] = i;
+            ans.push_back(nums[q[head]]);
+        }
+
+        delete[] q;
         return ans;
     }
 };
