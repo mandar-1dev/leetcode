db8aacdf1e8e889b8b65420dc4d4fbd7965545cb
Mandar Suryavanshi <mandar.acse4@gmail.com>
2026-08-24 18:52:24 +0530

    Time: 2 ms (71.57%) | Memory: 8 MB (99.58%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        static const int val[] = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        static const string sym[] = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        string ans;
+
+        for (int i = 0; i < 13; ++i) {
+            while (num >= val[i]) {
+                num -= val[i];
+                ans += sym[i];
+            }
+        }
+
+        return ans;
+    }
+};
